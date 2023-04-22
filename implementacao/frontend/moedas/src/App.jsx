1b64bc53d34fad35b6c8b4bcc312adc47179062b
Nikolas Louret <navlouret@sga.pucminas.br>
2023-04-22 16:40:24 -0300

    [UPDATE] Atualização dos arquivos root

--- implementacao/frontend/moedas/src/App.jsx
+++ implementacao/frontend/moedas/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+import CadastroAluno from "./pages/cadastro-aluno/CadastroAluno";
+import CadastroEmpresa from "./pages/CadastroEmpresa";
+import Perfil from "./pages/Perfil"
+
+// CSS
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <BrowserRouter>
+      <Routes>
+          <Route exact path="/cadastro-aluno" element={<CadastroAluno/>} />
+          <Route exact path="/cadastro-empresa" element={<CadastroEmpresa/>} />
+          <Route exact path="/perfil" element={<Perfil/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
