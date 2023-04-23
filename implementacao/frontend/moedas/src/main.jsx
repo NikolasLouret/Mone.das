563502e909b192a5873a26e330a8689fb6a4d44f
Nikolas Louret <navlouret@sga.pucminas.br>
2023-04-23 15:06:59 -0300

    [UPDATE] Adição das rotas das páginas

--- implementacao/frontend/moedas/src/main.jsx
+++ implementacao/frontend/moedas/src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { LoginProvider } from './context/LoginContext'
+
+//* Rotas
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//! User Router
+
+//* Pages User
+import Cadastro from './pages/cadastro/Cadastro.jsx'
+import Perfil from './pages/perfil/Perfil.jsx'
+
+const userRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/cadastrar", element: <Cadastro /> },
+      { path: "/perfil", element: <Perfil /> }
+    ]
+  }
+])
+
+//! Vantagem Router
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LoginProvider>
+      <RouterProvider router={ userRouter } />
+    </LoginProvider>
   </React.StrictMode>,
 )
