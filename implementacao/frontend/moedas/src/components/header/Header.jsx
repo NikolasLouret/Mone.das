d2629067f4ffa981e2dfb4448e666d26883bf031
Nikolas Louret <navlouret@sga.pucminas.br>
2023-04-23 16:19:51 -0300

    [UPDATE] Criação do header para navegação

---
+++ implementacao/frontend/moedas/src/components/header/Header.jsx
@@ -0,0 +1,83 @@
+import { Link } from 'react-router-dom'
+
+import { LoginContext } from '../../context/LoginContext'
+import { useEffect,useContext } from 'react'
+
+import './Header.css'
+
+const Header = () => {
+    const { currentUser } = useContext(LoginContext)
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <nav className="header">
+            <ul className="list-itens">
+                <div className="section-1">
+                    <li>
+                        <Link to="/"><img className="logo" src="public/logo_v1.svg" alt="mone.das" /></Link>
+                    </li>
+
+                    { currentUser && 
+                        <div className="itens-header">
+                            { currentUser.userType === "aluno" &&
+                                <>
+                                    <li>
+                                        <Link to="/">Ver Vantagens</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Ver extrato</Link>
+                                    </li>
+                                </>
+                            }
+
+                            { currentUser.userType === "professor" &&
+                                <>
+                                    <li>
+                                        <Link to="/">Ver extrato</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Distribuir moedas</Link>
+                                    </li>
+                                </>
+                            }
+
+                            { currentUser.userType === "empresa" &&
+                                <>
+                                    <li>
+                                        <Link to="/">Cadastrar Vantagens</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">Editar Vantagens</Link>
+                                    </li>
+                                </>
+                            }
+
+                        </div>
+                    }
+                </div>
+
+                <div className="section-2">
+                    { currentUser ?
+                        <li>
+                            <Link to="/perfil">Perfil</Link>
+                        </li>
+                        :
+                        <div className="register-login-header">
+                            <li>
+                                <Link to="/cadastrar">Cadatrar</Link>
+                            </li>
+                            <li>
+                                <Link to="/login">Entrar</Link>
+                            </li>
+                        </div>
+                    }
+                </div>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header
