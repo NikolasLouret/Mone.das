dce7ad3fc8d6b5d8381ae9551a5d868ce5642ed8
Nikolas Louret <navlouret@sga.pucminas.br>
2023-04-23 15:06:33 -0300

    [UPDATE] Mudança das rotas para a main

--- implementacao/frontend/moedas/src/App.jsx
+++ implementacao/frontend/moedas/src/App.jsx
@@ -1,21 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Pages
-import Cadastro from "./pages/cadastro/Cadastro";
-import Perfil from "./pages/Perfil"
+import { Outlet } from "react-router-dom"
 
 // CSS
 import './App.css'
 
 function App() {
-
   return (
-      <BrowserRouter>
-      <Routes>
-          <Route exact path="/cadastrar" element={<Cadastro/>} />
-          <Route exact path="/perfil" element={<Perfil/>} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+        <Outlet />
+    </div>
   )
 }
 
