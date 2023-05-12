35bcd8b0d4dc5b1ff68e4b49ebb1684082b6f6ea
GabrielLimaDeSouza <gabriel.souza.1354648@sga.pucminas.br>
2023-05-12 15:57:06 -0300

    [UPDATE] Rota get aluno by email

--- implementacao/frontend/moedas/src/pages/professor/EnviarMoedas.jsx
+++ implementacao/frontend/moedas/src/pages/professor/EnviarMoedas.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './EnviarMoedas.module.css'
 import Input from '../../components/inputs/Input'
 import Button from '../../components/buttons/Button'
 
 const EnviarMoedas = () => {
+    var [aluno,setAluno] = useState({})
+
+    function handleSubmit(e){
+        e.preventDefault()
+        var email = document.getElementById("inputMatricula").value;
+        var moedas = document.getElementById("inputMoedas").value;
+        var mensagem = document.getElementById("inputMensagem").value;
+
+        fetch(`http://localhost:3000/api/aluno/email/${email}`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(resp => resp.json())
+          .then(data => {
+            setAluno(data);
+            if (data && data.pessoa) {
+              fetch(`http://localhost:3000/api/carteira/transacao`, {
+                method: 'PUT',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                  "descricao": mensagem,
+                  "tipo": "Envio",
+                  "idDestinatario": data.pessoa.carteira,
+                  "idRemetente": "645bff468a6ecb1e80a07815",
+                  "valor": moedas
+                })
+              })
+              .then(resp => resp.json())
+              .catch(err => console.error(err));
+            }
+          })
+          .catch(err => console.error(err));
+    }
+
   return (
     <div className={styles.parent}>
         <div className={styles.leftScreen}>
@@ -16,7 +54,7 @@ const EnviarMoedas = () => {
             </div>
             <div className={styles.inputParent}>
                 <div className={styles.input}>
-                    <Input type="text" name="Matrícula" label="Matrícula" id="inputMatricula" />
+                    <Input type="text" name="Email" label="Email" id="inputMatricula" />
                 </div>
                 <div className={styles.inputInformations}>          
                     <p>Nome: </p>
@@ -33,10 +71,10 @@ const EnviarMoedas = () => {
                 </div>
             </div>
             <div>
-                <Input type="text" className={styles.inputSenha} name="Senha" label="Senha" id="inputSenha" />
+                <Input type="text" className={styles.inputSenha} name="Mensagem" label="Mensagem" id="inputMensagem" />
             </div>
             <div className={styles.divButton}>
-                <Button type="submit" className="submit" id="btnEnviar" children="Enviar" />
+                <Button type="submit" className="submit" id="btnEnviar" children="Enviar" onClick={handleSubmit}/>
             </div>
         </div>
         <div className={styles.rightScreen}>
