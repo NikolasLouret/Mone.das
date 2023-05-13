055d5f872620fe7664fcbef84ddb65d881615d2b
Nikolas Louret <navlouret@sga.pucminas.br>
2023-05-13 15:01:58 -0300

    [UPDATE] Adição de backdrop-filter no background

--- implementacao/frontend/moedas/src/App.jsx
+++ implementacao/frontend/moedas/src/App.jsx
@@ -9,13 +9,13 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className='absolute inset-0 flex justify-center'>
+      <div className='fixed inset-0 flex justify-center'>
         <div className='absolute opacity-50 bg-green bg-shape bg-blur' style={{ left: '-5rem', top: '10rem' }}></div>
         <div className='absolute opacity-70 bg-yellow bg-shape bg-blur' style={{ right: '5rem', top: '5rem' }}></div>
         <div className='absolute opacity-70 bg-blue bg-shape bg-blur' style={{ right: '15rem', top: '25rem' }}></div>
       </div>
 
-      <div className='App' style={{ padding: '10rem 5rem' }}>
+      <div className='App backdrop-filter' style={{ padding: '10rem 5rem' }}>
           <Header />
           <Outlet />
       </div>
