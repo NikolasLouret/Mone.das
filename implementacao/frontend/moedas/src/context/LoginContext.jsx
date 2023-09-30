84f4a05650a79c0d195f912a9924cd493a0d2c99
Nikolas Louret <nikoul.ret@gmail.com>
2023-09-29 23:19:44 -0300

    fix: correcao login ao criar conta e select form

--- implementacao/frontend/moedas/src/context/LoginContext.jsx
+++ implementacao/frontend/moedas/src/context/LoginContext.jsx
@@ -1,104 +1,116 @@
-import { useState, useEffect, createContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect, createContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-export const LoginContext = createContext();
+export const LoginContext = createContext()
 
 // eslint-disable-next-line react/prop-types
 export const LoginProvider = ({ children }) => {
-  const navigate = useNavigate();
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const url = "http://localhost:3000";
+	const navigate = useNavigate()
+	const [user, setUser] = useState(null)
+	const [loading, setLoading] = useState(true)
+	const url = 'http://localhost:3000'
 
-  useEffect(() => {
-    const recoveredUser = JSON.parse(localStorage.getItem("userLAB"));
+	useEffect(() => {
+		const recoveredUser = JSON.parse(localStorage.getItem('userLAB'))
 
-    if (recoveredUser) {
-      fetch(
-        `${url}/api/${recoveredUser.tipo.toLowerCase()}/pessoa/${
-          recoveredUser._id
-        }`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
-        .then((resp) => resp.json())
-        .then((data) => {
-          setUser(data);
-          setLoading(false);
-        });
-    } else setLoading(false);
-  }, []);
+		if (recoveredUser) {
+			fetch(`${url}/api/${recoveredUser.tipo.toLowerCase()}/pessoa/${recoveredUser._id}`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+				.then(resp => resp.json())
+				.then(data => {
+					setUser(data)
+					setLoading(false)
+				})
+		} else setLoading(false)
+	}, [])
 
-  const login = async (formData) => {
-    let data;
+	const loginCreateAccount = async loginUser => {
+		console.log(loginUser)
+		await fetch(`${url}/api/${loginUser.tipo.toLowerCase()}/pessoa/${loginUser.pessoa}`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then(resp => resp.json())
+			.then(data => {
+				setUser(data)
+				setLoading(false)
+				navigate('/')
+			})
+	}
 
-    if (!formData) {
-      return;
-    }
+	const login = async formData => {
+		let data
 
-    const objectForm = Object.fromEntries(formData);
+		if (!formData) {
+			return
+		}
 
-    await fetch(`${url}/api/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(objectForm),
-    })
-      .then((resp) => resp.json())
-      .then((resoJson) => {
-        data = resoJson;
-      });
+		const objectForm = Object.fromEntries(formData)
 
-    if (data.status === 201) {
-      const user = data.user;
-      localStorage.setItem("userLAB", JSON.stringify(user));
+		await fetch(`${url}/api/login`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(objectForm),
+		})
+			.then(resp => resp.json())
+			.then(respJson => {
+				data = respJson
+			})
 
-      await fetch(`${url}/api/${user.tipo.toLowerCase()}/pessoa/${user._id}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((resp) => resp.json())
-        .then((data) => {
-          setUser(data);
-          setLoading(false);
-          navigate("/");
-        });
-      return;
-    }
+		if (data.status === 201) {
+			const user = data.user
+			console.log(user)
+			localStorage.setItem('userLAB', JSON.stringify(user))
 
-    return data;
-  };
+			await fetch(`${url}/api/${user.tipo.toLowerCase()}/pessoa/${user._id}`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+				.then(resp => resp.json())
+				.then(data => {
+					setUser(data)
+					setLoading(false)
+					navigate('/')
+				})
+			return
+		}
 
-  const logout = () => {
-    localStorage.removeItem("userLAB");
+		return data
+	}
 
-    setUser(null);
-    navigate("/login");
-  };
+	const logout = () => {
+		localStorage.removeItem('userLAB')
 
-  const updateUser = (newUser) => {
-    setUser(newUser);
-  };
+		setUser(null)
+		navigate('/login')
+	}
 
-  return (
-    <LoginContext.Provider
-      value={{
-        authenticated: !!user,
-        user,
-        loading,
-        login,
-        logout,
-        updateUser,
-      }}
-    >
-      {children}
-    </LoginContext.Provider>
-  );
-};
+	const updateUser = newUser => {
+		setUser(newUser)
+	}
+
+	return (
+		<LoginContext.Provider
+			value={{
+				authenticated: !!user,
+				user,
+				loading,
+				login,
+				logout,
+				updateUser,
+				loginCreateAccount,
+			}}>
+			{children}
+		</LoginContext.Provider>
+	)
+}
